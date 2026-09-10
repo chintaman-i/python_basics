ab987cdf2712429c458d85635ace107828b3b00e
chintaman-i <chintamani@gmail.com>
2026-09-10 15:22:23 +0530

    Q1,Q2 solved

---
+++ Q1.py
@@ -0,0 +1,7 @@
+#check Wether the given number is odd or even
+#output Number is odd: False
+a=int(input("Enter Number to check: "))
+print("Number is odd:",a % 2 != 0)
+
+
+
---
+++ Q2.py
@@ -0,0 +1,6 @@
+#convert age to years
+#Output=3 years=1095 days
+a=int(input("Enter age to check: "))
+print(f"{a} years = {a*365} days")
+
+
