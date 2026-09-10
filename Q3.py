15ebb6dd48c475fe8d8f14ed9f6c06d572df486b
chintaman-i <chintamani@gmail.com>
2026-09-10 15:15:13 +0530

    Q3

---
+++ Q3.py
@@ -0,0 +1,10 @@
+#Write a program to convert minutes to hrs and print it
+#Example135 is 2 hours 15 minutes
+a=int(input("Enter minutes : "))
+
+hrs=a//60
+min=a%60
+print(f"{a} is {hrs} hours {min} minutes")
+
+
+
