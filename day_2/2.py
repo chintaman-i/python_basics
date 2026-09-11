f601820490fcc1804aec0b7c7c4233270ec0779c
chintaman-i <chintamani@gmail.com>
2026-09-11 15:32:42 +0530

    Into subfolders

--- day_2/2.py
+++ day_2/2.py
@@ -1,6 +1,6 @@
 #progrma to find a number is even or odd
 num=int(input("Enter a number: "))
 if num%2==0:
-    print(f"{num} is even")
+    print(" is even")
 else:
-    print(f"{num} is odd")
+    print(" is odd")
