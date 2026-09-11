452123b0334b2045a96fc5db64de9e3052858375
chintaman-i <chintamani@gmail.com>
2026-09-11 14:46:08 +0530

    Q6 solved

--- Q6.py
+++ Q6.py
@@ -1,6 +1,7 @@
 #write a program ot swap two variables without a third variable,using arithmetiv oprations
 #example: Before swap a= 10,b=20
 #after swap a= 20, b=10
+
 a=int(input("Enter value of a: "))
 b=int(input("Enter value of b: "))
 
