b03deb5346d9e2b44812dba578dec74bf5c56530
chintaman-i <chintamani8586@gmail.com>
2026-09-15 09:56:44 +0530

    problem Q1

--- day_3/1.py
+++ day_3/1.py
@@ -1,4 +1,13 @@
-while True:
-    sting=input("enter a string:")
-    print(string)
-    
+#while True:
+ #   string=input("enter a string:")
+  #  print(string)
+
+correct_pass="Hello123"
+not_found=True
+
+while not_found:
+    passw=input("Enter the password:")
+    if passw==correct_pass:
+        not_found=False
+print("Access granted")
+
